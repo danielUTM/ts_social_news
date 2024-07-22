b24da7ff506ecf5c44ed064dda9deb1e6793331d
danielUTM <danielcampbell.327@icloud.com>
2024-07-22 18:07:34 +0100

    Removed spaces replaced with tabs

--- src/getClient.ts
+++ src/getClient.ts
@@ -6,11 +6,11 @@ const USERNAME = process.env.USERNAME;
 
 module.exports.getClient = async () => {
   const client = new Client({
-	    user: USERNAME,
-	    password: 'password',
-	    host: 'localhost',
-	    port: 5432,
-	    database: 'social_news',
+		user: USERNAME,
+		password: 'password',
+		host: 'localhost',
+		port: 5432,
+		database: 'social_news',
     });
   await client.connect();
   return client;
